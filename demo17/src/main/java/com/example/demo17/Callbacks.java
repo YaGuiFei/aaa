0ff1b587f57c621840c9f1bca934e55522e77273
WXL <18807820020@139.com>
2020-06-23 21:30:56 +0800

    V3

---
+++ demo17/src/main/java/com/example/demo17/Callbacks.java
@@ -0,0 +1,7 @@
+package com.example.demo17;
+
+import android.os.Bundle;
+
+public interface Callbacks {
+    void onItemSelected(int id , Bundle bundle);
+}
---
+++ demo17/src/main/java/com/example/demo17/Viewitem_Activity.java
@@ -0,0 +1,15 @@
+package com.example.demo17;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+public class Viewitem_Activity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viewitem_);
+    }
+}
